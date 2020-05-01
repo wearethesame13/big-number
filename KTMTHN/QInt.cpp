38d475b35f9d2ba04b4f28868333ac4cd9f47a2e
Đạt Lê Thọ <wearethesame13>
2020-05-01 23:11:58 +0700

    add h operators

--- KTMTHN/QInt.cpp
+++ KTMTHN/QInt.cpp
@@ -1,1 +1,134 @@
 #include "QInt.h"
+QInt& QInt::operator=(const QInt& b)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		data[i] = b.data[i];
+	}
+	return *this;
+}
+
+QInt operator-(const QInt a)
+{
+	QInt result;
+	for (int i = 0; i < 4; i++)
+	{
+		result.data[i] = a.data[i] * (-1);
+	}
+	return result;
+}
+bool operator<(const QInt& a, const QInt& b)
+{
+	for (int i = 3; i >= 0; i--)
+	{
+		if (a.data[i] < b.data[i])
+		{
+			return true;
+		}
+		else if (a.data[i] > b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] == b.data[i])
+		{
+			continue;
+		}
+	}
+	return false;
+}
+bool operator==(const QInt& a, const QInt& b)
+{
+	for (int i = 3; i >= 0; i--)
+	{
+		if (a.data[i] < b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] > b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] == b.data[i])
+		{
+			continue;
+		}
+	}
+	return true;
+}
+
+bool operator>(const QInt& a, const QInt& b)
+{
+	for (int i = 3; i >= 0; i--)
+	{
+		if (a.data[i] < b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] > b.data[i])
+		{
+			return true;
+		}
+		else if (a.data[i] == b.data[i])
+		{
+			continue;
+		}
+	}
+	return false;
+}
+bool operator<(const QInt& a, const QInt& b)
+{
+	for (int i = 3; i >= 0; i--)
+	{
+		if (a.data[i] < b.data[i])
+		{
+			return true;
+		}
+		else if (a.data[i] > b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] == b.data[i])
+		{
+			continue;
+		}
+	}
+	return false;
+}
+bool operator>=(const QInt& a, const QInt& b)
+{
+	for (int i = 3; i >= 0; i--)
+	{
+		if (a.data[i] < b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] > b.data[i])
+		{
+			return true;
+		}
+		else if (a.data[i] == b.data[i])
+		{
+			continue;
+		}
+	}
+	return true;
+}
+bool operator<=(const QInt& a, const QInt& b)
+{
+	for (int i = 3; i >= 0; i--)
+	{
+		if (a.data[i] < b.data[i])
+		{
+			return true;
+		}
+		else if (a.data[i] > b.data[i])
+		{
+			return false;
+		}
+		else if (a.data[i] == b.data[i])
+		{
+			continue;
+		}
+	}
+	return true;
+}
--- KTMTHN/QInt.h
+++ KTMTHN/QInt.h
@@ -3,4 +3,12 @@ class QInt
 {
 public:
 	int data[4];
+	QInt& operator=(const QInt& b);
+	friend QInt operator-(const QInt a);
+	friend bool operator<(const QInt& a, const QInt& b);
+	friend bool operator==(const QInt& a, const QInt& b);
+	friend bool operator>(const QInt& a, const QInt& b);
+	friend bool operator<=(const QInt& a, const QInt& b);
+	friend bool operator>=(const QInt& a, const QInt& b);
+
 };
