192822a571cebd3701133685416f7d5b7d7807e9
Vu Le Tuan <43137499+harukishima@users.noreply.github.com>
2020-05-07 06:34:39 +0700

    add header

--- DoAnKTMT/QInt.h
+++ DoAnKTMT/QInt.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <cmath>
 class QInt
 {
 public:
@@ -13,3 +15,9 @@ void setBitQInt(QInt &x, bool* bit);
 QInt BinToDec(bool* bit);
 void ScanQInt(QInt& x);
 void PrintQInt(QInt x);
+char* BinToHex(bool* bit); //Chuyển đổi QInt nhị phân sang thập lục phân, bit có độ dài 128
+char GroupBinToHex(bool* bit); //Chuyển dãy 4 bit thành kí tự Hex, bit có độ dài 4
+char HexIndex(int index);
+char* DecToHex(QInt x); //Chuyển đổi QInt thập phân sang thập lục phân
+bool getBitInt(int value, int pos); //Lấy bit tại vị trí pos từ trái sang
+char* vectorToStr(std::vector<char> x); //Chuyển đổi vector char về chuỗi
