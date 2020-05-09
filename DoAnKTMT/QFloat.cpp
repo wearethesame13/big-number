41a96f9817460bcfe99861d01648ad97ee6966a9
Vu Le Tuan <43137499+harukishima@users.noreply.github.com>
2020-05-09 16:25:33 +0700

    Update QFloat.cpp

--- DoAnKTMT/QFloat.cpp
+++ DoAnKTMT/QFloat.cpp
@@ -41,3 +41,13 @@ QFloat QFloat :: BinToDec(bool *bit)
 	}
 	return *this;
 }
+
+bool* DecToBin(Qfloat x)
+{
+	bool* result = new bool[128];
+	for (int i = 0; i < 128; i++)
+	{
+		result[i] = getBitInt(x.data[i / 32], i % 32);
+	}
+	return result;
+}
