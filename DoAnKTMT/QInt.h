b766e74cd3b4a0d39436241e351f6249d1e90e96
Vu Le Tuan <43137499+harukishima@users.noreply.github.com>
2020-05-09 20:30:33 +0700

    Update QInt.h

--- DoAnKTMT/QInt.h
+++ DoAnKTMT/QInt.h
@@ -48,9 +48,12 @@ std::string convertDectoBin(QInt x);
 char* BinToHex(bool* bit); //Chuyển đổi QInt nhị phân sang thập lục phân, bit có độ dài 128
 char GroupBinToHex(bool* bit); //Chuyển dãy 4 bit thành kí tự Hex, bit có độ dài 4
 char HexIndex(int index);
+int rHexIndex(char hex);
 char* DecToHex(QInt x); //Chuyển đổi QInt thập phân sang thập lục phân
 bool getBitInt(int value, int pos); //Lấy bit tại vị trí pos từ trái sang
 char* vectorToStr(std::vector<char> x); //Chuyển đổi vector char về chuỗi
+bool* HexToBin(std::string x);
+QInt HexToDec(std::string x);
 
 std::string HexToBin(std::string Hex);
 std::string ReverseHexIndex(char i);
