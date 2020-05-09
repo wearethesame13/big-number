451bed3e82da1b4369973eaeea5cd71c45283fa6
Phan Xuan Bao <62206740+baroxfitxhcmus@users.noreply.github.com>
2020-05-09 15:51:55 +0700

    Update FileProcess.h

--- DoAnKTMT/FileProcess.h
+++ DoAnKTMT/FileProcess.h
@@ -1,8 +1,12 @@
-#pragma once
+
 #pragma once
 #include "QInt.h"
 #include <string>
 #include <sstream>
+#include"QFloat.h"
+#include<fstream>
 using namespace std;
 string convert(string line);
 string operate(string line);
+void convertQFloat(string strIn);
+void readQFloat();
